0029aa994c21aa4dc61dcb1b743a7782cd72f6a7
Yohai Bar Sinai <6164157+yohai@users.noreply.github.com>
2019-02-21 16:35:24 -0500

    explicit

--- pde_superresolution/weno_test.py
+++ pde_superresolution/weno_test.py
@@ -48,5 +48,74 @@ class WenoTest(parameterized.TestCase):
     utr = weno.calculate_time_derivative(-ur)
     assert np.allclose(ut, -utr[::-1])
 
+  def test_curvature_calculation(self):
+    u = np.random.randn(40)
+
+
+def ExplicitLectureNotesWENO(v):
+  '''
+  This follows Procedure 2.2 as verbatim and as explicitly as possible.
+   It is for test purposes and is probably very inefficient
+  '''
+  epsilon = 1e-6
+
+  # Table 2.1 in the lecture notes for k=3
+  _crj_matrix = {
+    -1: np.array([11 / 6, -7 / 6, 1 / 3]),
+    0: np.array([1 / 3, 5 / 6, -1 / 6]),
+    1: np.array([-1 / 6, 5 / 6, 1 / 3]),
+    2: np.array([1 / 3, -7 / 6, 11 / 6]),
+  }
+
+  def c_rj(r, j):
+    return _crj_matrix[r][j]
+
+  def cbar_rj(r, j):  # see unnumbered equation below Eq. (2.10)
+    return c_rj(r - 1, j)
+
+  ######################################################################################################
+  # 1. Obtain the k reconstructed values ... in (2.51), based on the stencils (2.51) for r = 0, ..., k-1
+  n = len(v)
+  v_r_i_plus_half = {k: sum(c_rj(r, j) * v[ (i - r + j) % n] for j in range(k)) for r in range(k)}
+
+  # Also obtain the k reconstructed values ...
+  v_r_i_minus_half = {k: sum(cbar_rj(r, j) * v[(i - r + j) % n] for j in range(k)) for r in range(k)}
+
+  ######################################################################################################
+  # 2. Find the constants d_r such that ...
+  d = np.array([3/10, 3/5, 1/10])
+  # by symmetry, dbar[r] = d[k-1-r]
+  dbar = d[::-1]
+
+  ######################################################################################################
+  # 3. Find the smooth indicators βr in (2.61)...
+  beta_1 = (
+          (13 / 12) * np.array([u[i] - 2 * u[(i + 1) % n] + u[(i + 2) % n] for i in range(n)]) ** 2 +
+          (1 / 4) * np.array([3 * u[i] - 4 * u[(i + 1) % n] + u[(i + 2) % n] for i in range(n)]) ** 2
+  )
+  beta_2 = (
+          (13 / 12) * np.array([u[(i - 1) % n] - 2 * u[(i) % n] + u[(i + 1) % n] for i in range(n)]) ** 2 +
+          (1 / 4) * np.array([u[(i - 1) % n] - u[(i + 1) % n] for i in range(n)]) ** 2
+  )
+  beta_3 = (
+          (13 / 12) * np.array([u[(i - 2) % n] - 2 * u[(i - 1) % n] + u[(i) % n] for i in range(n)]) ** 2 +
+          (1 / 4) * np.array([u[i - 2] - 4 * u[(i - 1) % n] + 3 * u[(i) % n] for i in range(n)]) ** 2
+  )
+  beta_r = np.stack([beta_1, beta_2, beta_3])
+
+  ######################################################################################################
+  # 4. Form the weights ωr and ωr using (2.58)-(2.59) and ...
+  alpha_r = d/(epsilon + beta_r)
+  omega_r= alpha_r/alpha_r.sum()
+
+  alphabar_r =  dbar/(epsilon + beta_r)
+  omegabar_r = alphabar_r / alphabar_r.sum()
+
+  ######################################################################################################
+  # 5. Find the (2k − 1)-th order reconstruction ...
+  v_minus_i_plus_half = sum(omega_r[r] * v_r_i_plus_half[r] for r in range(k))
+  v_plus_i_minus_half = sum(omegabar_r[r] * v_r_i_minus_half[r] for r in range(k))
+
+
 if __name__ == '__main__':
   absltest.main()
